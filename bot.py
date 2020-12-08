affd2d2f602d87b201620216e8f617a1ec73b50d
violet <github@violet-mica.com>
2020-12-07 22:29:31 -0800

    added viewer greeter that remembers user across reboots (sqlite3)

--- bot.py
+++ bot.py
@@ -7,12 +7,28 @@
 import os # for importing env vars for the bot to use
 from twitchio.ext import commands
 from envbash import load_envbash
+import datetime
+import sqlite3
 load_envbash('env.txt')
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
 owner_username = os.environ['OWNER_ID']
 bot_nick = os.environ['BOT_NICK']
 channel = os.environ['CHANNEL']
+
+conn = sqlite3.connect('twitch_bot.db')
+try:
+    conn.execute('''
+    create table viewers (
+        name text unique
+        , last_seen text
+        , first_seen text
+    )
+    ''')
+    conn.commit()
+except sqlite3.OperationalError:
+    conn.rollback()
+    pass
 
 bot = commands.Bot(
     # set up the bot
@@ -31,6 +47,34 @@ async def event_ready():
     # await ws.send_privmsg(channel, f"/me has landed!")
     # await ws.send_privmsg(channel, f"👋")
 
+
+def saw_user_today(username):
+    _saw_user_today = False
+    today = datetime.date.today()
+
+    cursor = conn.execute(
+        """SELECT *
+            from viewers
+            where name = ?
+                and last_seen > ?""",
+        (username, today)
+    )
+    user = cursor.fetchone()
+    if user:
+        _saw_user_today = True
+
+    return _saw_user_today
+
+def update_saw_user(username):
+    now = str(datetime.datetime.now())
+    conn.execute(
+        f'''INSERT INTO viewers values (?,?,?)
+            ON CONFLICT(name) DO UPDATE SET last_seen=?;
+        ''',
+        (username, now, now, now)
+    )
+    conn.commit()
+
 @bot.event
 async def event_message(ctx):
     'Runs every time a message is sent in chat.'
@@ -38,9 +82,15 @@ async def event_message(ctx):
     # make sure the bot ignores itself and the streamer
     if ctx.author.name.lower() == bot_nick.lower():
         return
-    elif ctx.author.name == owner_username:
-        print(ctx.content)
-        await ctx.channel.send(ctx.content)
+    # elif ctx.author.name == owner_username:
+    #     print(ctx.content)
+    #     await ctx.channel.send(ctx.content)
+
+    if not saw_user_today(ctx.author.name):
+        await ctx.channel.send(f'Welcome, {ctx.author.name}!')
+
+    update_saw_user(ctx.author.name)
+
 
 def print_startup_message_file():
     startup_file_path = os.path.join(this_dir, 'startup_message.txt')
