3efd965ef1771d1d93d84472a979609a702962b5
Violet Eldridge <github@violet-mica.com>
2023-11-18 23:43:16 -0800

    remove unnecessary file; specify port as 5000 (default was 8000)

--- start_server.py
+++ start_server.py
@@ -1,4 +1,4 @@
 import subprocess
 
-command = ["poetry", "run", "uvicorn", "twitch_bot.server:app", "--host", "localhost", "--reload"]
+command = ["poetry", "run", "uvicorn", "twitch_bot.server:app", "--host", "localhost", "--reload", "--port", "5000"]
 subprocess.run(command)
