258645eb91fedcc3039a8a667acbbfd6b725a585
Violet Eldridge <github@violet-mica.com>
2025-01-05 17:45:17 -0800

    feat(api): add pantheon to unban users

--- commandbay/resources/pantheon.py
+++ commandbay/resources/pantheon.py
@@ -37,6 +37,30 @@ def ban_user(username: str):
     return {"message": f"User '{username}' has been banned."}
 
 
+@pantheon_router.put("/unban/{username}", status_code=status.HTTP_200_OK)
+def unban_user(username: str):
+    """
+    Unban a user by removing them from the banned_users.txt file.
+    """
+    banned_users = get_banned_users()
+    found_and_removed = False
+    for i, user in enumerate(banned_users):
+        if username.lower() == user.lower():
+            banned_users.pop(i)
+            found_and_removed = True
+            break
+
+    if not found_and_removed:
+        return {"message": f"'{username}' wasn't banned."}
+
+    # If not present, append the new username to the file
+    with open(BAN_FILE, "w", encoding="utf-8") as f:
+        for user in banned_users:
+            print(user, file=f)
+
+    return {"message": f"User '{username}' has been unbanned."}
+
+
 @pantheon_router.get("/ban")
 def list_banned_users():
     """
